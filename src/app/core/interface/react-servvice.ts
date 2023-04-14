25cae97d6d0106f856d6ae01a569844a1b978d56
orsys <orsys@orsys.fr>
2023-04-14 13:53:37 +0200

    Interface B

--- src/app/core/interface/react-servvice.ts
+++ src/app/core/interface/react-servvice.ts
@@ -1,2 +1,9 @@
-export interface ReactiveService {
+import { BehaviorSubject } from "rxjs";
+
+export interface ReactiveService<DataType, CommandType> {
+
+  data$ : BehaviorSubject<DataType>;
+
+  execute(command:CommandType, payload:any):void;
+
 }
