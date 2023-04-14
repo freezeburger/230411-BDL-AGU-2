f1de20d09d58802b5e44fa932ae907d1dfba8cef
orsys <orsys@orsys.fr>
2023-04-14 07:08:29 +0200

    Bad Subscribe

--- src/app/pages/page-messages/page-messages.component.ts
+++ src/app/pages/page-messages/page-messages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BdlMessage } from 'src/app/core/interface/bdl-message';
+import { MessageService } from 'src/app/core/services/message.service';
 
 @Component({
   selector: 'app-page-messages',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-messages.component.scss']
 })
 export class PageMessagesComponent {
+
+  public viewMessages:BdlMessage[] = []
+
+  constructor( public messageSrv:MessageService){
+    this.messageSrv.messages$.subscribe( messages => this.viewMessages = messages)
+  }
+
 
 }
