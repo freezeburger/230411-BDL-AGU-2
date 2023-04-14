df6f44b9a12546304de8605d9623a3f82f8ba604
orsys <orsys@orsys.fr>
2023-04-14 07:01:10 +0200

    Observable

--- src/app/core/services/message.service.ts
+++ src/app/core/services/message.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { BdlMessage } from '../interface/bdl-message';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class MessageService {
 
-  constructor() { }
+   /**
+   * Observable
+   *
+   * factory
+   * Subject - Valeurs APRES subscription
+   * BehaviorSubject - Valeurs APRES subscription ET dernière valeur AVANT
+   * ReplaySubject - Valeurs APRES subscription ET TOUTES valeurs AVANT
+   */
+
+  public messages$ = new BehaviorSubject<BdlMessage[]>([]);
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  private API = 'http://localhost:5050/messages';
+  public getData(){
+    this.http.get<BdlMessage[]>(this.API)
+             .subscribe( data => this.messages$.next(data.reverse()) );
+  }
+
+  public sendData( message = { text:'Default Text', date:Date.now() } ){
+    this.http.post(this.API, message )
+             .subscribe( data => this.messages$.next([data,...this.messages$.value]) );
+  }
 }
