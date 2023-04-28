0060ab0ac6e802cf7a1bab4c323ed91b317aa2b3
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-04-28 16:17:55 +0200

    Update

--- projects/wc-bdl-auth/src/app/app.module.ts
+++ projects/wc-bdl-auth/src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
+import { BdlAuthComponent, BdlAuthModule } from 'projects/bdl-auth/src/public-api';
 
 @NgModule({
   declarations: [
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: []
+    BrowserModule,
+    BdlAuthModule
+  ]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+
+  constructor( private injector:Injector){}
+
+  ngDoBootstrap(){
+    const customElementDefinition= createCustomElement(BdlAuthComponent, { injector: this.injector });
+    customElements.define('wc-auth', customElementDefinition )
+  }
+}
